da8dabb4927ffb1e3ffc0b8883ea81288ce23037
Irina Iskra <iskrairina.a@gmail.com>
2023-10-08 21:50:38 +0300

    check deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello ulasp</h1>
+      <Routes>
+        <Route index element={<WelcomePage />} redirectTo="/main" />
+      </Routes>
+      {/* <h1>Hello ulasp</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import Footer from "../../components/Footer/Footer";
+const WelcomePage = () => {
+  return (
+    <>
+      <h1>Welcome to the app!</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default WelcomePage;
