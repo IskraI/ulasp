783498069b767cc980b1939802f29ebaf0368c71
Irina Iskra <iskrairina.a@gmail.com>
2023-10-12 16:50:27 +0300

    var

---
+++ src/styles/vars.js
@@ -0,0 +1,15 @@
+export const colors = {
+  primaryColor: "#FFF3BF",
+  mainFontColor: "#17161C",
+  btnColor: "rgba(164, 188, 212, 0.3)",
+  activeBtnColor: "#8CACD7",
+  accentHoverColor: "#FFF3BF",
+  primaryWhite: "#ffffff",
+  BgModal: "#7F99C0",
+  inputBgGrayColor: "rgba(0, 0, 0, 0.4)",
+  mileageInputBorderColor: "#8A8A8933",
+};
+
+export const transition = {
+  duration: "250ms linear",
+};
