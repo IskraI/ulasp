0a89b11b518a0ac3c5ddb33b94f767d4b899a27f
Alexon7 <alenkaalexon@gmail.com>
2023-10-16 18:17:43 +0200

    add sprite and navAdmin

---
+++ src/components/NavMenu/NavMenu.styled.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Nav = styled.nav`
+  
+`;
+
+export const List = styled.ul`
+  
+`;
+
+export const Item = styled.li`
+ 
+`;
+
+export const NavLink = styled.a`
+  
+`;
