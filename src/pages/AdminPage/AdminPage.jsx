c92aa773b291d7ffbb27b12d9fbd6ed515f82ae7
Irina Iskra <iskrairina.a@gmail.com>
2023-10-21 18:24:57 +0300

    prob

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,22 +1,27 @@
-
-import { ContactInfo } from '../../components/ContactInfo/ContactInfo';
-import { NavMenu } from '../../components/NavMenu/NavMenu'
-import symbol from '../../assets/symbol.svg'
-import {
-  LogOutLink,
-  Exit
-} from './AdminPage.styled'
+import { ContactInfo } from "../../components/ContactInfo/ContactInfo";
+import { NavMenu } from "../../components/NavMenu/NavMenu";
+import symbol from "../../assets/symbol.svg";
+import { LogOutLink, Exit } from "./AdminPage.styled";
+import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 
 const AdminPage = () => {
   console.log("AdminUsersPage");
-  return( <>
-    <NavMenu />
-    <ContactInfo />
-     <Exit><svg className="icon" width="24" height="24" >
-            <use  href={`${symbol}#icon-exit`}></use> </svg>
-    <LogOutLink href="/admin/signin"> Вихід </LogOutLink></Exit>
-  </>);
-
+  return (
+    <>
+      <Suspense fallback={"Loader"}>
+        <Outlet />
+      </Suspense>
+      <NavMenu />
+      <ContactInfo />
+      <Exit>
+        <svg className="icon" width="24" height="24">
+          <use href={`${symbol}#icon-exit`}></use>{" "}
+        </svg>
+        <LogOutLink href="/admin/signin"> Вихід </LogOutLink>
+      </Exit>
+    </>
+  );
 };
 
 export default AdminPage;
