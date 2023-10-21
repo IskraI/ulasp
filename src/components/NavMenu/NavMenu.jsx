24568453219f6ffdb81f4475483815c312e23fb5
Irina Iskra <iskrairina.a@gmail.com>
2023-10-21 18:29:15 +0300

    Update NavMenu.jsx

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,13 +1,7 @@
 // import { useLocation } from "react-router-dom";
 
-import {
-    Nav,
-    List,
-    Item,
-    NavigationLink,
-   } from './NavMenu.styled';
-import symbol from '../../assets/symbol.svg'
-  
+import { Nav, List, Item, NavigationLink } from "./NavMenu.styled";
+import symbol from "../../assets/symbol.svg";
 
 import icon from "../../assets/symbol-defs.svg";
 
@@ -17,42 +11,40 @@ export const NavMenu = () => {
   return (
     <Nav>
       <List>
-
         <Item>
           <svg className="icon" width="24" height="24">
             <use href={`${symbol}#icon-cabinet`}></use>
           </svg>
-          <NavLink href="/cabinet">Кабінет</NavLink>
+          <NavigationLink href="/cabinet">Кабінет</NavigationLink>
         </Item>
         <Item>
           <svg className="icon" width="24" height="24">
             <use href={`${symbol}#icon-message`}></use>
           </svg>
-          <NavLink href="/messages">Повідомлення</NavLink>
+
+          <NavigationLink href="/messages">Повідомлення</NavigationLink>
         </Item>
         <Item>
           {" "}
           <svg className="icon" width="24" height="24">
             <use href={`${symbol}#icon-list`}></use>
           </svg>
-          <NavLink href="/user">Користувачі</NavLink>
+          <NavigationLink href="/user">Користувачі</NavigationLink>
         </Item>
         <Item>
           {" "}
           <svg className="icon" width="24" height="24">
             <use href={`${symbol}#icon-online`} width="24" height="24"></use>
           </svg>
-          <NavLink href="/online">Онлайн</NavLink>
+          <NavigationLink href="/online">Онлайн</NavigationLink>
         </Item>
         <Item>
           <svg className="icon" width="24" height="24">
             <use href={`${symbol}#icon-analytics`}></use>
           </svg>
-          <NavLink href="/analytics">Аналітика</NavLink>
+          <NavigationLink href="/analytics">Аналітика</NavigationLink>
         </Item>
       </List>
     </Nav>
-
-
   );
 };
