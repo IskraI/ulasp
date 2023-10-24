2b4c7a17506c7004c874f2dce91d9b0fbc733a0b
IskraI <IskraI@users.noreply.github.com>
2023-10-24 19:22:27 +0000

    deploy: dae69dc8ecbd84414d1d2da776c36b610d4e5faa

--- assets/AdminCabinetPage-09bc749d.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-73aa09ed.js";const n=()=>e.jsx(e.Fragment,{children:"CabinetAdminPAge"});export{n as default};
---
+++ assets/AdminCabinetPage-e4ab83f3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e7711d6f.js";const n=()=>e.jsx(e.Fragment,{children:"CabinetAdminPAge"});export{n as default};
