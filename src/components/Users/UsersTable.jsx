ec31fbc22ec7f1c25dfb27bf522b7e2d6c0faa0a
Irina Iskra <iskrairina.a@gmail.com>
2023-11-02 12:25:14 +0200

    update navmenu svg

--- src/components/Users/UsersTable.jsx
+++ src/components/Users/UsersTable.jsx
@@ -1,41 +1,35 @@
 import React from "react";
 
- const UsersTable = ({ users }) => {
-  return (<>
-  <table>
-      <thead>
-        <tr>
-         
-          <th> Name</th>
-        
-               
-         
-        
-          <th>Day of Birthday</th>
-          <th>Tel Number</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, index) => (
-          <tr key={index}>
-                     <td>{user.firstName}</td>
-            <td>{user.lastName} {user.fatherName}</td>
-                 
-          
-            <td>{user.dayOfBirthday}</td>
-            <td>{user.telNumber}</td>
-            <td>{user.email}</td>
+const UsersTable = ({ users }) => {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th> Name</th>
+
+            <th>Day of Birthday</th>
+            <th>Tel Number</th>
+            <th>Email</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.firstName}</td>
+              <td>
+                {user.lastName} {user.fatherName}
+              </td>
+
+              <td>{user.dayOfBirthday}</td>
+              <td>{user.telNumber}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-   
   );
 };
 
 export default UsersTable;
-
-
-     
