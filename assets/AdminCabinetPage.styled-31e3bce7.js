9fcc21a392893b3905bbe27e001c39f80c58ad3e
IskraI <IskraI@users.noreply.github.com>
2023-11-03 21:48:41 +0000

    deploy: c59e2e1f9bb40ba7ad667ee0321702a7625bfe28

---
+++ assets/AdminCabinetPage.styled-31e3bce7.js
@@ -0,0 +1,127 @@
+import{n as t,c as o}from"./index-2a903511.js";const n=t.div`
+  display: flex;
+`,i=t.input`
+  display: flex;
+  width: 219px;
+  height: 40px;
+  padding: 0px 8px;
+  justify-content: flex-end;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 10px;
+  border: 1px solid ${o.mainFontColor};
+  background-color: transparent;
+  /* margin-right: 64px; */
+  margin-left: auto;
+  margin-top: 29px;
+  ::placeholder {
+    text-align: right;
+    color: rgba(0, 0, 0, 0.4);
+  }
+`,a=t.table`
+  table-layout: fixed;
+  /* width: 100%; */
+  border-collapse: collapse;
+  margin-left: 127px;
+
+  tbody {
+    tr:nth-of-type(odd) {
+      background-color: rgba(234, 234, 234, 0.32);
+    }
+  }
+
+  thead th:nth-of-type(1) {
+    width: 30%;
+  }
+
+  thead th:nth-of-type(2) {
+    width: 20%;
+  }
+
+  thead th:nth-of-type(3) {
+    width: 15%;
+  }
+
+  thead th:nth-of-type(4) {
+    width: 15%;
+  }
+
+  thead th:nth-of-type(5) {
+    width: 20%;
+  }
+
+  tbody td:nth-of-type(1) {
+    width: 30%;
+  }
+
+  tbody td:nth-of-type(2) {
+    width: 20%;
+  }
+
+  tbody td:nth-of-type(3) {
+    width: 15%;
+  }
+
+  thead td:nth-of-type(4) {
+    width: 15%;
+  }
+
+  tbody td:nth-of-type(5) {
+    width: 20%;
+  }
+`;t.th`
+  color: ${o.mainFontColor};
+  font-size: 16px;
+  font-weight: 600;
+  margin-top: 23px;
+  text-align: left;
+    /* flex: 1; */
+`;t.tr`
+  display: flex;
+  /* flex: 1; */
+  /* gap: 129px; */
+  height: 54px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+padding-left: 16px;
+`;t.td`
+text-align: left;
+
+ /* flex: 1; */
+  // Здесь вы можете определить стили для ячеек таблицы
+         
+ 
+`;const l=t.h3`
+  color: ${o.mainFontColor};
+  font-size: 22px;
+  margin-top: 43px;
+  /* margin-left: 111px; */
+  line-height: calc(26.4 / 22);
+  font-weight: 400;
+  line-height: 1.28;
+`,r=t.p`
+  color: ${o.mainFontColor};
+  font-size: 24px;
+  font-weight: 500;
+`,h=t.p`
+  color: ${o.mainFontColor};
+  font-size: 24px;
+  font-weight: 500;
+`;t.button`
+  color: ${o.mainFontColor};
+ font-family: Inter;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+background-color: transparent;
+ outline: none;
+  border: none;
+
+`;const d=t.h2`
+ color:   ${o.mainFontColor};
+font-size: 24px;
+font-weight: 500;
+
+`;export{i as I,n as S,l as T,r as a,h as b,a as c,d};
