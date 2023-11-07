8864027bb2a3ed2bfe435c027e5be62d25530bc7
Irina Iskra <iskrairina.a@gmail.com>
2023-11-07 21:48:15 +0200

    on/off btn

--- src/pages/AdminPage/AdminPage.styled.jsx
+++ src/pages/AdminPage/AdminPage.styled.jsx
@@ -2,6 +2,8 @@ import styled from "@emotion/styled";
 
 export const AdminSection = styled.div`
   display: flex;
+  /* justify-content: space-evenly; */
+
 `;
 export const AdminSubpage = styled.div`
   display: flex;
