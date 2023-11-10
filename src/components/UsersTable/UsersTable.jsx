5c73de419b88a973f65e52d7d1b5d504762a5c76
Irina Iskra <iskrairina.a@gmail.com>
2023-11-10 18:54:25 +0200

    date on table

--- src/components/UsersTable/UsersTable.jsx
+++ src/components/UsersTable/UsersTable.jsx
@@ -1,68 +1,76 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import {
-   Table,
+  Table,
   TableRow,
   TableCell,
   RowTitle,
-  DetailsBtn
- } from "../SearchUsers/SearchUsers.styled";
- import {Button} from "../Button/Button"
-const UsersTable = ({ users,  visibleColumns}) => {
+  DetailsBtn,
+} from "../SearchUsers/SearchUsers.styled";
+import { Button } from "../Button/Button";
+
+const UsersTable = ({ users, visibleColumns }) => {
 
   const formatDate = (dateString) => {
-    const year = dateString.getFullYear();
-            const month = String(dateString.getMonth() + 1).padStart(2, "0");
-            const day = String(dateString.getDate()).padStart(2, "0");
-           return `${year}/${month}/${day}`;
+    const originalDate = new Date("2023-11-10T14:58:20.594Z");
+
+    const day = String(originalDate.getDate()).padStart(2, "0");
+    const month = String(originalDate.getMonth() + 1).padStart(2, "0"); // Месяцы начинаются с 0
+    const year = originalDate.getFullYear();
+    const formattedDate = `${day}.${month}.${year}`;
+    return formattedDate;
   };
 
-
-      return (
+  return (
     <>
       <Table>
-      <thead>
-        <TableRow>
-          {visibleColumns.map((column, columnIndex) => (
-            <RowTitle key={column.key} isFirstColumn={columnIndex === 0}>{column.label}</RowTitle>
-          ))}
-          
-        </TableRow>
-      </thead>
-      
+        <thead>
+          <TableRow>
+            {visibleColumns.map((column, columnIndex) => (
+              <RowTitle key={column.key} isFirstColumn={columnIndex === 0}>
+                {column.label}
+              </RowTitle>
+            ))}
+          </TableRow>
+        </thead>
+
         <tbody>
           {users.map((user, index) => {
-    
-          return (
-            <TableRow key={index}>
-              {visibleColumns.map((column, columnIndex) => (
-                
-              <TableCell key={column.key} isFirstColumn={columnIndex === 0}>
-                {
-                // column.type === 'date'
-                //   ? formatDate(user[column.key])
-                //   : 
-                column.type === 'name'
-                ? user.name? user.name : user.firstName +" "+user.lastName 
-                  : column.type === 'link'
-                  
-
-                  ? (
-  <Link to="/admin/users/carduser"> 
-    <DetailsBtn type="link">картка</DetailsBtn>
-  </Link>
-)
-
-                  :column.key === 'boolean'
-                  ? user[column.key]
-                  :column.key === 'sendEmail'
-                  ? <Button type="button" text= "Відправити" padding = "8px" onClick ={()=>console.log("send email")}></Button>
-                  : user[column.key]}
+            return (
+              <TableRow key={index}>
+                {visibleColumns.map((column, columnIndex) => (
+                  <TableCell key={column.key} isFirstColumn={columnIndex === 0}>
+                    {
+                      column.type === 'date'
+                        ? formatDate(user[column.key])
+                        :
+                      column.type === "name" ? (
+                        user.name ? (
+                          user.name
+                        ) : (
+                          user.firstName + " " + user.lastName
+                        )
+                      ) : column.type === "link" ? (
+                        <Link to="/admin/users/carduser">
+                          <DetailsBtn type="link">картка</DetailsBtn>
+                        </Link>
+                      ) : column.key === "boolean" ? (
+                        user[column.key]
+                      ) : column.key === "sendEmail" ? (
+                        <Button
+                          type="button"
+                          text="Відправити"
+                          padding="8px"
+                          onClick={() => console.log("send email")}
+                        ></Button>
+                      ) : (
+                        user[column.key]
+                      )
+                    }
                   </TableCell>
-            ))}
-
-            
-            </TableRow>
-          )})}
+                ))}
+              </TableRow>
+            );
+          })}
         </tbody>
       </Table>
     </>
