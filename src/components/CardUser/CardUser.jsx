ffba1c564c4bd41bab00aa9857bcd4278424ec71
Alexon7 <alenkaalexon@gmail.com>
2023-11-11 19:03:59 +0100

    add card user and Nav

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -1,9 +1,57 @@
 import TabNavigation from "../TabNavigation/TabNavigation";
+import UserCardForm from "./CardUserForm";
+import { Button } from "../Button/Button";
+import symbol from "../../assets/symbol.svg";
 
 const CardUser = () => {
   return <>
-    <TabNavigation/>
-        <h3>Картка користувача</h3>
+    <TabNavigation />
+    <UserCardForm />
+         <Button
+              type="button"
+              padding ="8px"
+      height="48px"
+      width="198px"
+            text={
+          <>
+            <svg className="icon" width="24" height="24" style={{ marginRight: '8px' }}>
+              <use href={`${symbol}#icon-analytics`}></use>
+            </svg>
+            Звіт
+          </>
+        }
+      display="flex"
+    />
+    <Button
+              type="button"
+              padding ="8px"
+      height="48px"
+      width="310px"
+     text="Відправити посилання"
+    />
+    <Button
+              type="button"
+              padding ="8px"
+      height="48px"
+      width="198px"
+     text="Розблокувати"
+    />
+   <Button
+              type="button"
+              padding ="8px"
+      height="48px"
+      width="310px"
+     text={
+          <>
+            <svg className="icon" width="24" height="24" style={{ marginRight: '8px' }}>
+              <use href={`${symbol}#icon-del-basket`}></use>
+            </svg>
+            Видалити користувача
+          </>
+        }
+      display="flex"
+    />
+    
     </>
                    
 };
