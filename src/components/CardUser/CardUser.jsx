df4301626604797a2d1eb21a0c6082a63bf6b84c
Alexon7 <alenkaalexon@gmail.com>
2023-11-20 13:15:46 +0100

    toggele  block user

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -5,7 +5,7 @@ import symbol from "../../assets/symbol.svg";
 import { useState } from "react";
 import { Modal } from "../Modal/Modal";
 import { useParams } from "react-router-dom";
-import { useGetUserByIdQuery,  useDelUserByIdMutation } from "../../redux/dataUsersSlice";
+import { useGetUserByIdQuery,  useDelUserByIdMutation, useUnblockUserByIdMutation } from "../../redux/dataUsersSlice";
 import {
   ButtonContainer,
   TextModal,
@@ -17,7 +17,8 @@ const CardUser = () => {
   const navigate = useNavigate();
   const { id } = useParams();
      const { data: user, error, isLoading } = useGetUserByIdQuery(id);
-  const [dispatchDel, {isLoading: isLoadingDel} ] = useDelUserByIdMutation();
+  const [dispatchDel, { isLoading: isLoadingDel }] = useDelUserByIdMutation();
+  const [dispatchUnblock, { isLoading: isLoadingUnblock }] = useUnblockUserByIdMutation(); 
   const [activeModal, setActiveModal] = useState(null);
   const handleShowModal = (modalContent) => {
     setActiveModal(modalContent);
@@ -29,6 +30,15 @@ const CardUser = () => {
 
   const handleDeleteUser = async () => {
     dispatchDel(id)
+      .unwrap()
+      .then(() => {
+        navigate("/admin/users");
+      })
+      .catch((error) => console.log(error.data.message));
+  };
+
+   const handleUnblockUser = async () => {
+    dispatchUnblock(id)
       .unwrap()
       .then(() => {
         navigate("/admin/users");
@@ -73,7 +83,7 @@ const CardUser = () => {
           padding="8px 30px"
           display="block"
           height="48px"
-          text="Розблокувати"
+          text={user && user.status === true ? "Заблокувати" : "Розблокувати"}
           marginleft={"auto"}
           onClick={() => handleShowModal('unBlock')}
         />
@@ -124,8 +134,9 @@ const CardUser = () => {
         padding="8px 30px"
         display="block"
       height="48px"
-          text="Розблокувати"
-           marginleft='31px' 
+           text={user && user.status === true ? "Заблокувати" : "Розблокувати" }
+              marginleft='31px' 
+              onClick={handleUnblockUser}
           />
           </ModalBtnContainer>
         </Modal>
