c897433b408d6dc13b1202d3ad46c1c0e9e94bed
IskraI <IskraI@users.noreply.github.com>
2023-11-28 21:21:25 +0000

    deploy: 29e34a91d2f5b213fa084a8d30aeb313efe0da28

--- assets/EditorCabinetPage-7863efec.js
+++ assets/EditorCabinetPage-7863efec.js
@@ -1,4 +1,4 @@
-import{n as e,c as j,j as t,d as m,e as y,f as T}from"./index-efc6c80c.js";import{S as b,b as f}from"./Statistic.styled-e601ff8b.js";const w=e.table`
+import{n as e,c as j,j as t,d as m,e as y,f as T}from"./index-9f577523.js";import{S as b,b as f}from"./Statistic.styled-921ce808.js";const w=e.table`
   text-align: left;
   font-size: 18px;
   table-layout: fixed;
--- assets/SearchUsers.styled-7113dac4.js
+++ assets/SearchUsers.styled-7113dac4.js
@@ -1,4 +1,4 @@
-import{n as t,N as n,c as e}from"./index-efc6c80c.js";const a=t(n)`
+import{n as t,N as n,c as e}from"./index-9f577523.js";const a=t(n)`
 
  &.active {text-decoration: underline}
 
