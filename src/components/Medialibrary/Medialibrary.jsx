6a98c81cbbaf6a4237ae9569fa6d2cb15cb6b4c3
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-11-29 20:18:23 +0200

    dsdsdsa

--- src/components/Medialibrary/Medialibrary.jsx
+++ src/components/Medialibrary/Medialibrary.jsx
@@ -1,6 +1,16 @@
-const Medialibrary = () => {
-    return <>MediatekaUser</>;
-  };
-  
-  export default Medialibrary;
-  
+import Genres from "../Genres/Genres";
+import LatestPlaylists from "../PlayLists/PlayLists";
+import NewSongs from "../NewSongs/NewSongs";
+
+const MediaLibrary = () => {
+  return (
+    <>
+      <Genres displayPlayer={"none"} />
+      <Genres displayPlayer={"none"} />
+      <LatestPlaylists />
+      <NewSongs />
+    </>
+  );
+};
+
+export default MediaLibrary;
