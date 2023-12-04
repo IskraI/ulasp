2590288bc0343820147445223f3ca2ad3e1d64d6
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-12-05 00:05:31 +0200

    update

--- src/components/EditorComponents/Genres/Genres.jsx
+++ src/components/EditorComponents/Genres/Genres.jsx
@@ -24,7 +24,7 @@ const Genres = ({
   const [showModal, setShowModal] = useState(false);
   
   
-  const [genre, setGenre] = useState(null);
+  // const [genre, setGenre] = useState(null);
   
   
   const toogleModal = () => {
