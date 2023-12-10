7438cedea1a3f4f831faa0ed6dbbaae97eddff6a
Irina Iskra <iskrairina.a@gmail.com>
2023-12-11 00:31:54 +0200

    fix form

--- src/components/AdminComponents/ButtonSwitch/ButtonSwitch.jsx
+++ src/components/AdminComponents/ButtonSwitch/ButtonSwitch.jsx
@@ -1,72 +1,111 @@
 import {
-    ButtonCustomSwitch,ContentButton
-  } from "../ButtonSwitch/ButtonSwitch.styled";
-  import { useAccessUserUpdateByIdMutation } from "../../../redux/dataUsersSlice";
+  ButtonCustomSwitch,
+  ContentButton,
+} from "../ButtonSwitch/ButtonSwitch.styled";
+import { useAccessUserUpdateByIdMutation } from "../../../redux/dataUsersSlice";
 
-  import { useState } from "react";
+import { useState } from "react";
 
-
-export const ButtonSwitch = ({isTrue,idUser })=>{
+export const ButtonSwitch = ({
+  isTrue,
+  type,
+  idUser,
+  onClick,
+  form = false,
+}) => {
   const [typeOfAccess, setTypeOfAccess] = useState(isTrue);
+  console.log("form", form);
 
-const [dispatchAccess, { isLoading: isLoadingAccess }] = useAccessUserUpdateByIdMutation();
+  const [dispatchAccess, { isLoading: isLoadingAccess }] =
+    useAccessUserUpdateByIdMutation();
 
-const handleSwitchAccess = (idUser) => {
+  const handleSwitchAccess = (idUser) => {
     dispatchAccess(idUser)
       .unwrap()
       .then(() => {
-        setTypeOfAccess(prev =>!prev)
-    console.log('dispatchAccess',idUser )
+        setTypeOfAccess((prev) => !prev);
+        console.log("dispatchAccess", idUser);
       })
       .catch((error) => console.log(error.data.message));
   };
 
-    return(<>
+  return (
+    <>
+      {!form && (
+        <>
+          {isLoadingAccess && (
+            <ButtonCustomSwitch type="button">Loading</ButtonCustomSwitch>
+          )}
 
-      {isLoadingAccess &&  <ButtonCustomSwitch   type="button"
- >
-
-         Loading
-   
-  </ButtonCustomSwitch>}
-
-      {!isLoadingAccess && typeOfAccess && (
-  <ButtonCustomSwitch   type="button"
-  isTrue={typeOfAccess}
-
-  onClick={()=>handleSwitchAccess(idUser)}>
- 
-         On
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="13"
-      height="13"
-      viewBox="0 0 13 13"
-      fill="none"
-    >
-      <circle cx="6.5" cy="6.5" r="6" fill="#8CACD7" />
-    </svg>
-
-  </ButtonCustomSwitch>
-) }
-{!isLoadingAccess && !typeOfAccess && 
- (
-  <ButtonCustomSwitch type="button"
-  isTrue={typeOfAccess}
-  onClick={()=>handleSwitchAccess(idUser)}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="13"
-      height="13"
-      viewBox="0 0 13 13"
-      fill="none"
-    >
-      <circle cx="6.5" cy="6.5" r="6" fill="#FFF3BF" />
-    </svg>
-    Off
-  </ButtonCustomSwitch>
-)}
-      </>)}
-      
-
-
+          {!isLoadingAccess && typeOfAccess && (
+            <ButtonCustomSwitch
+              type="button"
+              isTrue={typeOfAccess}
+              onClick={() => handleSwitchAccess(idUser)}
+            >
+              On
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+              >
+                <circle cx="6.5" cy="6.5" r="6" fill="#8CACD7" />
+              </svg>
+            </ButtonCustomSwitch>
+          )}
+          {!isLoadingAccess && !typeOfAccess && (
+            <ButtonCustomSwitch
+              type="button"
+              isTrue={typeOfAccess}
+              onClick={() => handleSwitchAccess(idUser)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+              >
+                <circle cx="6.5" cy="6.5" r="6" fill="#FFF3BF" />
+              </svg>
+              Off
+            </ButtonCustomSwitch>
+          )}
+        </>
+      )}
+      {form && (
+        <>
+          {isTrue ? (
+            <ButtonCustomSwitch type={type} isTrue={isTrue} onClick={onClick}>
+              On
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+              >
+                <circle cx="6.5" cy="6.5" r="6" fill="#8CACD7" />
+              </svg>
+            </ButtonCustomSwitch>
+          ) : (
+            <ButtonCustomSwitch type={type} isTrue={isTrue} onClick={onClick}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+              >
+                <circle cx="6.5" cy="6.5" r="6" fill="#FFF3BF" />
+              </svg>
+              Off
+            </ButtonCustomSwitch>
+          )}
+        </>
+      )}
+    </>
+  );
+};
