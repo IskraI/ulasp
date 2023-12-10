7c1d95feeb18ab51145830e0b345def33bc9d120
IskraI <IskraI@users.noreply.github.com>
2023-12-10 22:13:42 +0000

    deploy: 83124839f288c434ee45b4e6ee66df3164fc13bd

--- assets/SearchUsers.styled-84bf80f7.js
+++ assets/SearchUsers.styled-84bf80f7.js
@@ -1,4 +1,4 @@
-import{n as t,X as n,c as e}from"./index-57fd124c.js";const a=t(n)`
+import{n as t,X as n,c as e}from"./index-64c26f05.js";const a=t(n)`
 
  &.active {text-decoration: underline}
 
--- assets/UserCabinetPage-008f9f50.js
+++ assets/UserCabinetPage-008f9f50.js
@@ -1,4 +1,4 @@
-import{n,c as t,a,j as o,b as m,g as b}from"./index-57fd124c.js";import"./Statistic.styled-5044eb26.js";n.p`
+import{n,c as t,a,j as o,b as m,g as b}from"./index-64c26f05.js";import"./Statistic.styled-d00031b1.js";n.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
