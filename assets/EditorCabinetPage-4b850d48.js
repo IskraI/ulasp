d22dea36531feae3c164e3512d36452969a9055c
IskraI <IskraI@users.noreply.github.com>
2023-12-22 12:40:33 +0000

    deploy: df94b60ef7b1c008de33c67444dc3450334b77e5

--- assets/EditorCabinetPage-4b850d48.js
+++
@@ -1,6 +0,0 @@
-import{n as x,c as h,d as j,e as k,f as T,j as s,L as y,h as m,P as p}from"./index-befe0c90.js";import{S as L,b as e}from"./Statistic.styled-81605860.js";const C=x.p`
-  font-size: 24px;
-  font-weight: 500;
-  line-height: 1.21;
-  color: ${h.mainFontColor};
-`,F=["","Назва пісні","Виконавець","Тривалість","Жанр","Плейлист",""],f=()=>{const{data:i,error:o,isLoading:n}=j(),{data:c,error:l,isFetching:d}=k(),{data:t,isLoading:u,error:a,isFetching:r,isSuccess:g}=T();return s.jsxs(s.Fragment,{children:[s.jsx(C,{children:" Кабінет редактора"}),s.jsxs(L,{children:[s.jsxs(e,{children:[!d&&!l&&c.countTracks,s.jsx("br",{})," Доданої музики"]}),s.jsxs(e,{children:[!n&&!o&&i.countPlaylists,s.jsx("br",{})," Створених плейлистів"]})]}),r&&s.jsx(y,{}),!r&&!a&&s.jsxs(s.Fragment,{children:[s.jsx(m,{title:" Остання додана музика",tracks:t,isLoading:u,error:a,isFetching:r,isSuccess:g,rows:F}),s.jsx(p,{tracks:t})]})]})};export{f as default};
