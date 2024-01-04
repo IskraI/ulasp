7a00cf6ddb99a6223aaac4e90543966443eb1e7c
Alexon7 <alenkaalexon@gmail.com>
2024-01-04 14:36:08 +0100

    add btn sort style

--- src/pages/UserPage/AllTracksUser/AllTracksUser.styled.jsx
+++ src/pages/UserPage/AllTracksUser/AllTracksUser.styled.jsx
@@ -1,8 +1,11 @@
 import styled from "@emotion/styled";
 
 export const BtnSort = styled.button`
+position:absolute;
  background: none;
   border: none;
   padding: 5px;
   cursor: pointer;
+  margin-left: 350px;
+  margin-top:  15px;
 `;
