b0390d0c64de6c90ab48736757b5c0daddee254c
Anton-ck <Anton-ck@users.noreply.github.com>
2024-01-15 16:31:34 +0000

    deploy: fac9cf76c773649a2712442af9542b2a2366557f

---
+++ assets/Statistic.styled-e7ef8319.js
@@ -0,0 +1,20 @@
+import{n as t,f as a}from"./index-4566aec9.js";const e=t.div`
+  display: flex;
+  gap: 8px;
+  /* margin-left: 111px; */
+  margin-top: 24px;
+  margin-bottom: 29px;
+`,r=t.p`
+  display: flex;
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: ${a.primaryColor};
+  color: ${a.mainFontColor};
+  text-align: center;
+  font-size: 22px;
+  line-height: calc(26.4 / 22);
+`,i=t(r)`
+  margin-right: 68px;
+`;export{e as S,r as a,i as b};
