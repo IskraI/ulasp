bdc4284941a571f558ebf5aaa8dc99a9c9536dc8
Irina Iskra <iskrairina.a@gmail.com>
2024-02-03 18:49:02 +0200

    genre in track of genre

--- src/pages/UserPage/PlaylistPageUser/TracksInGenre.jsx
+++ src/pages/UserPage/PlaylistPageUser/TracksInGenre.jsx
@@ -1,37 +1,44 @@
 import TabNavigation from "../../../components/TabNavigation/TabNavigation";
 import TracksTable from "../../../components/UserMediaComponent/TracksTable/TracksTableUser";
-import {  useGetTracksByGenreIdQuery  } from "../../../redux/tracksUserSlice";
+import { useGetTracksByGenreIdQuery } from "../../../redux/tracksUserSlice";
 import { BtnSort } from "../AllTracksUser/AllTracksUser.styled";
 import symbol from "../../../assets/symbol.svg";
 import { useState, useEffect, useId, useRef } from "react";
-import NavMusic from "../../../components/UserMediaComponent/NavMusic/NavMusic"
-import { useParams} from "react-router-dom";
-import Player from '../../../components/Player/Player';
-import  DropDownTracksInGenres  from "../../../components/DropDownGeners/DropDownTracksInGener";
+import NavMusic from "../../../components/UserMediaComponent/NavMusic/NavMusic";
+import { useParams } from "react-router-dom";
+import Player from "../../../components/Player/Player";
+import DropDownTracksInGenres from "../../../components/DropDownGeners/DropDownTracksInGener";
 
 // const RowsTitle = ["", "Назва пісні", "Виконавець", "Тривалість", "Жанр", ""];
 
 const AllTracksUser = () => {
-   const id = useId();
+  const id = useId();
   const BaseInputRef = useRef(null);
-const { genreId } = useParams();
-const [checkedMainCheckBox, setCheckedMainCheckBox] = useState(false);
+  const { genreId } = useParams();
+  const [checkedMainCheckBox, setCheckedMainCheckBox] = useState(false);
   // const {
   //   data: allTracks,
 
   //   error: errorLoadingAllTracks,
   //   isFetching: isFetchingAllTracks,
   // } = useGetAllTracksforUserQuery();
-  
-  const { data: allTracks, error: errorLoadingAllTracks,
-    isFetching: isFetchingAllTracks, isSuccess} = useGetTracksByGenreIdQuery(genreId);
-    
+
+  const {
+    data: allTracks,
+    error: errorLoadingAllTracks,
+    isFetching: isFetchingAllTracks,
+    isSuccess,
+  } = useGetTracksByGenreIdQuery(genreId);
+
   const links = [
-    { path: `/user/medialibrary/genres/${genreId}/playlists`, title: "Плейлисти" },
+    {
+      path: `/user/medialibrary/genres/${genreId}/playlists`,
+      title: "Плейлисти",
+    },
     { path: `/user/medialibrary/genres/${genreId}/tracks`, title: "Пісні" },
   ];
 
-   const rows = () => {
+  const rows = () => {
     const RowsTitle = [
       {
         title: (
@@ -109,49 +116,50 @@ const [checkedMainCheckBox, setCheckedMainCheckBox] = useState(false);
     return RowsTitle;
   };
 
-     const [sortAlphabetically, setSortAlphabetically] = useState(false);
+  const [sortAlphabetically, setSortAlphabetically] = useState(false);
 
   const handleSortClick = () => {
-    
     setSortAlphabetically(!sortAlphabetically);
-    };
-    
-     const sortedTracks = allTracks
-    ? [...allTracks].sort((a, b) => {
+  };
+
+  const sortedTracks = allTracks
+    ? [...allTracks.tracks].sort((a, b) => {
         const titleA = (a.trackName || "").toUpperCase();
         const titleB = (b.trackName || "").toUpperCase();
         return sortAlphabetically ? titleA.localeCompare(titleB) : 0;
       })
     : [];
-  
-      console.log('allTracks', allTracks)
-       return (
-      <>
-           <TabNavigation /> 
-           <DropDownTracksInGenres currentGenreId={genreId}/>
-           <NavMusic links={links} />
-       <BtnSort onClick={handleSortClick}><svg width="24" height="24" >
-                <use href={`${symbol}#icon-sort`}></use>
-              </svg></BtnSort>   
+
+  return (
+    <>
+      <TabNavigation />
+      <DropDownTracksInGenres currentGenreId={genreId} />
+      <NavMusic links={links} />
+      <BtnSort onClick={handleSortClick}>
+        <svg width="24" height="24">
+          <use href={`${symbol}#icon-sort`}></use>
+        </svg>
+      </BtnSort>
       {isFetchingAllTracks && <p>Загружаемся.....</p>}
-           {!isFetchingAllTracks && !errorLoadingAllTracks && (
-             <>
-               <TracksTable
-                  title={"In playlist"}
+      {!isFetchingAllTracks && !errorLoadingAllTracks && (
+        <>
+          {console.log("allTracks.tracks", allTracks.tracks)}
+          <TracksTable
+            title={"In playlist"}
             showTitle={false}
             marginTopWrapper={"24px"}
-            isInPlayList={false}
+            isInPlayList={true}
             // playListId={allTracks.playlist._id}
-            // playListGenre={allTracks.playlist.playlistGenre}
-                  tracks={sortedTracks}
-          error={errorLoadingAllTracks}
-          isFetching={isFetchingAllTracks}
-          isSuccess = {isSuccess}
-           display="none"
+            playListGenre={allTracks.playlistGenre}
+            tracks={sortedTracks}
+            error={errorLoadingAllTracks}
+            isFetching={isFetchingAllTracks}
+            isSuccess={isSuccess}
+            display="none"
             rows={rows()}
-             />
-              <Player tracks={allTracks} />
-             </>
+          />
+          <Player tracks={allTracks.tracks} />
+        </>
       )}
     </>
   );
--- src/redux/tracksUserSlice.js
+++ src/redux/tracksUserSlice.js
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants/constants";
 
-
-
 export const tracksUserApi = createApi({
   reducerPath: "tracksUserApi",
   baseQuery: fetchBaseQuery({
@@ -18,34 +16,39 @@ export const tracksUserApi = createApi({
   tagTypes: ["Tracks"],
 
   endpoints: (builder) => ({
-   
     getAllTracksforUser: builder.query({
-       query: (page = "", limit = "") => ({
+      query: (page = "", limit = "") => ({
         url: `/user/tracks/latestTracks?${page && `page=${page}`} & ${
           limit && `limit=${limit}`
         }`,
       }),
-     
-      providesTags: (_result, _err, id) => [{ type: "Tracks", id },
-        { type: "Playlists", id },],
+
+      providesTags: (_result, _err, id) => [
+        { type: "Tracks", id },
+        { type: "Playlists", id },
+      ],
     }),
 
-     getTracksByGenreId: builder.query({
+    getTracksByGenreId: builder.query({
       query: (genreId) => ({
         url: `user/genre/${genreId}/tracks`,
       }),
-      providesTags: (_result, _error, genreId) => [{ type: "Tracks", genreId }],
-     }),
-     
-      deleteTrack: builder.mutation({
+      invalidatesTags: ["Tracks"],
+      // providesTags: (_result, _error, genreId) => [{ type: "Tracks", genreId }],
+    }),
+
+    deleteTrack: builder.mutation({
       query: (id) => ({
         url: `/user/tracks/delete/${id}`,
         method: "DELETE",
       }),
       invalidatesTags: ["Tracks"],
     }),
-    
   }),
 });
 
-export const {  useGetAllTracksforUserQuery, useGetTracksByGenreIdQuery, useDeleteTrackMutation } = tracksUserApi;
+export const {
+  useGetAllTracksforUserQuery,
+  useGetTracksByGenreIdQuery,
+  useDeleteTrackMutation,
+} = tracksUserApi;
