09d5a9128522579e29509f170b37e8470ceb346e
Anton-ck <Anton-ck@users.noreply.github.com>
2024-02-11 14:35:17 +0000

    deploy: 44c5a25219889fbbdaf731044420cb6e3a75b454

--- assets/EditorCabinetPage-3269bdf9.js
+++ assets/EditorCabinetPage-3269bdf9.js
@@ -1,4 +1,4 @@
-import{n as o,f as n,k as c,j as t,L as u,E as h,l as x,m as p}from"./index-62886b98.js";import{S as d,b as r}from"./Statistic.styled-0831ac47.js";const S=o.p`
+import{n as o,f as n,k as c,j as t,L as u,E as h,l as x,m as p}from"./index-9cfe6d45.js";import{S as d,b as r}from"./Statistic.styled-481dc62d.js";const S=o.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
--- assets/UserCabinetPage-e64f6490.js
+++ assets/UserCabinetPage-e64f6490.js
@@ -1,4 +1,4 @@
-import{n,f as t,g as a,j as o,h,i as g}from"./index-62886b98.js";import"./Statistic.styled-0831ac47.js";n.p`
+import{n,f as t,g as a,j as o,h,i as g}from"./index-9cfe6d45.js";import"./Statistic.styled-481dc62d.js";n.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
