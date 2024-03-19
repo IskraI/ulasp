e64b8ff0522ef90871ea07a228b3ec1fe6c46d20
IskraI <IskraI@users.noreply.github.com>
2024-03-19 21:28:51 +0000

    deploy: b3f6062188935a121136245bcd1913527a601101

--- assets/Statistic.styled-a8f39fa4.js
+++ assets/Statistic.styled-a8f39fa4.js
@@ -1,4 +1,4 @@
-import{n as t,f as o}from"./index-b6d52129.js";const i=t.div`
+import{n as t,f as o}from"./index-8b510db0.js";const i=t.div`
   display: flex;
   gap: 8px;
   /* margin-left: 111px; */
