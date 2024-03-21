8d71ad05189506750004ba01ca20737f655fe907
Irina Iskra <iskrairina.a@gmail.com>
2024-03-21 22:36:01 +0200

    UserCabinetPage formatted

--- src/components/UserCabinetPage/UserCabinetPage.styled.jsx
+++ src/components/UserCabinetPage/UserCabinetPage.styled.jsx
@@ -1,49 +1,41 @@
 import styled from "@emotion/styled";
 import { colors } from "../../styles/vars";
 
-
-
 export const EditorText = styled.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
   color: ${colors.mainFontColor};
-`; 
+`;
 
 export const TitleFirst = styled.h2`
- color: ${colors.mainFontColor};
+  color: ${colors.mainFontColor};
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
   margin-top: 24px;
- margin-bottom: 24px;
-`; 
+  margin-bottom: 24px;
+`;
 
 export const TitleSecond = styled.h2`
- color: ${colors.mainFontColor};
+  color: ${colors.mainFontColor};
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
   margin-top: 18px;
- 
-`; 
+`;
 
 export const DataUser = styled.p`
- color: ${colors.mainFontColor};
+  color: ${colors.mainFontColor};
   font-size: 20px;
   font-weight: 400;
   line-height: 1.21;
- margin-top: 11px;
-`; 
-
+  margin-top: 11px;
+`;
 
 export const FormContainer = styled.div`
- display: flex;
- gap: 135px;
- position: relative;
-`; 
-
-
-
-
-
+  display: flex;
+  justify-content: space-around;
+  gap: 135px;
+  position: relative;
+`;
