2208ca6f737b1e1ff61192e45316fbadbfbc3d6d
IskraI <IskraI@users.noreply.github.com>
2024-03-29 22:04:18 +0000

    deploy: e8f9e40edac3ecea595eb4908a8662d835df6d08

--- assets/UserCabinetPage-7343f602.js
+++ assets/UserCabinetPage-7343f602.js
@@ -1,4 +1,4 @@
-import{n as o,f as n,g as L,h as V,r,j as e,B as D,M as j,i as T,k as W,l as Y,R as H,m as N,o as Q,p as G,q as J,s as K,t as X}from"./index-e3ec2940.js";import"./Statistic.styled-ddf43856.js";o.p`
+import{n as o,f as n,g as L,h as V,r,j as e,B as D,M as j,i as T,k as W,l as Y,R as H,m as N,o as Q,p as G,q as J,s as K,t as X}from"./index-bc9ec484.js";import"./Statistic.styled-9bbf508e.js";o.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
