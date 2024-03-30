3f55260eb7e48952a3b35e51b3518d54c0d52be0
Irina Iskra <iskrairina.a@gmail.com>
2024-03-30 23:58:53 +0200

    dots

--- src/components/UserMediaComponent/TracksTable/TracksTable.jsx
+++ src/components/UserMediaComponent/TracksTable/TracksTable.jsx
@@ -1,136 +1,136 @@
-/* eslint-disable react/prop-types */
-import {
-  TableCell,
-  RowTitle,
-  TrackCover,
-  TableStyle,
-  THeadStyle,
-  TrStyle,
-  LatestTracks,
-  TracksNotFound,
-  TracksTableWrapper,
-  
-} from "./TracksTable.styled";
-import { useDispatch, useSelector } from "react-redux";
-import { BASE_URL, ERROR_NOT_FOUND } from "../../../constants/constants";
-import symbol from "../../../assets/symbol.svg";
-import { Button } from "../../Button/Button";
-import { setPreloadSrcPlayer, stopPlay } from "../../../redux/playerSlice";
-import { getPlayerState } from "../../../redux/playerSelectors";
+// /* eslint-disable react/prop-types */
+// import {
+//   TableCell,
+//   RowTitle,
+//   TrackCover,
+//   TableStyle,
+//   THeadStyle,
+//   TrStyle,
+//   LatestTracks,
+//   TracksNotFound,
+//   TracksTableWrapper,
 
-const TracksTable = ({ rows, tracks, isLoading, error, display, title,  showTitle, marginTopWrapper, }) => {
-  const sToStr = (sec) => {
-    sec = Math.round(sec);
-    let m = Math.trunc(sec / 60) + "";
-    sec = (sec % 60) + "";
-    return m.padStart(2, 0) + ":" + sec.padStart(2, 0);
-  };
+// } from "./TracksTable.styled";
+// import { useDispatch, useSelector } from "react-redux";
+// import { BASE_URL, ERROR_NOT_FOUND } from "../../../constants/constants";
+// import symbol from "../../../assets/symbol.svg";
+// import { Button } from "../../Button/Button";
+// import { setPreloadSrcPlayer, stopPlay } from "../../../redux/playerSlice";
+// import { getPlayerState } from "../../../redux/playerSelectors";
 
-  const dispatch = useDispatch();
-  const playerState = useSelector(getPlayerState);
-  const tracksTableProps = {
-    showTitle: showTitle ? "table-caption" : "none",
-    marginTop: marginTopWrapper ? `${marginTopWrapper}` : "auto",
-    showData: rows.map((rows) => (rows.showData ? true : false)),
-  };
+// const TracksTable = ({ rows, tracks, isLoading, error, display, title,  showTitle, marginTopWrapper, }) => {
+//   const sToStr = (sec) => {
+//     sec = Math.round(sec);
+//     let m = Math.trunc(sec / 60) + "";
+//     sec = (sec % 60) + "";
+//     return m.padStart(2, 0) + ":" + sec.padStart(2, 0);
+//   };
 
-  const isLoadedTracks = playerState.isLoaded;
-  
-  const playMusic = () => {
-    const trackURL = tracks.map((track) => {
-      const newObject = {
-        trackURL: track.trackURL,
-        artist: track.artist,
-        trackName: track.trackName,
-      };
-      return newObject;
-    });
+//   const dispatch = useDispatch();
+//   const playerState = useSelector(getPlayerState);
+//   const tracksTableProps = {
+//     showTitle: showTitle ? "table-caption" : "none",
+//     marginTop: marginTopWrapper ? `${marginTopWrapper}` : "auto",
+//     showData: rows.map((rows) => (rows.showData ? true : false)),
+//   };
 
-    dispatch(setPreloadSrcPlayer(trackURL));
-  };
+//   const isLoadedTracks = playerState.isLoaded;
 
-  const stopMusic = () => {
-    dispatch(stopPlay());
-  };
+//   const playMusic = () => {
+//     const trackURL = tracks.map((track) => {
+//       const newObject = {
+//         trackURL: track.trackURL,
+//         artist: track.artist,
+//         trackName: track.trackName,
+//       };
+//       return newObject;
+//     });
 
-  return (
-    <>
-      {error && <TracksNotFound>{ERROR_NOT_FOUND}</TracksNotFound>}
-      {isSuccess && tracks?.length === 0 && !isLoading && !error && (
-        <TracksNotFound>Музика ще не завантажена</TracksNotFound>
-      )}
+//     dispatch(setPreloadSrcPlayer(trackURL));
+//   };
 
-      {tracks?.length !== 0 && !isLoading && !error && (
-        <>
-          <Button
-            onClick={() => (!isLoadedTracks ? playMusic() : stopMusic())}
-            type={"button"}
-            width={"250px"}
-            height={"50px"}
-            padding={"16px"}
-            margintop={"12px"}
-            text={isLoadedTracks ? "Зупинити" : "Грати музику"}
-            showIcon={"true"}
-            // icon={`${symbol}#icon-play`}
-            icon={
-              isLoadedTracks
-                ? `${symbol}#icon-stop-play`
-                : `${symbol}#icon-play`
-            }
-          />
-           <TracksTableWrapper marginTop={tracksTableProps.marginTop}>
-            <LatestTracks showTitle={tracksTableProps.showTitle}>{title}</LatestTracks>
-            <TableStyle>
-              <THeadStyle>
-                <tr>
-                  {rows.map((row, rowindex) => {
-                    return <RowTitle key={rowindex}>{row}</RowTitle>;
-                  })}
-                </tr>
-              </THeadStyle>
-              <tbody>
-                {tracks.map(
-                  ({
-                    _id,
-                    trackPictureURL,
-                    trackName,
-                    artist,
-                    trackDuration,
-                    trackGenre,
-                    playList,
-                  }) => {
-                    return (
-                      <TrStyle key={_id}>
-                        <TableCell>
-                          <TrackCover
-                            src={BASE_URL + "/" + trackPictureURL}
-                            alt={trackName}
-                            width={55}
-                          />
-                        </TableCell>
+//   const stopMusic = () => {
+//     dispatch(stopPlay());
+//   };
 
-                        <TableCell>{trackName}</TableCell>
-                        <TableCell>{artist}</TableCell>
-                        <TableCell>{sToStr(trackDuration)}</TableCell>
-                        <TableCell>{trackGenre}</TableCell>
+//   return (
+//     <>
+//       {error && <TracksNotFound>{ERROR_NOT_FOUND}</TracksNotFound>}
+//       {isSuccess && tracks?.length === 0 && !isLoading && !error && (
+//         <TracksNotFound>Музика ще не завантажена</TracksNotFound>
+//       )}
 
-                        <TableCell style={{ display }}>
-                          {playList?.playListName}
-                        </TableCell>
-                        <TableCell style={{ display }}><svg width="24" height="24" stroke="#888889">
-                          <use href={`${symbol}#icon-dots`}></use>
-                        </svg></TableCell>
-                      </TrStyle>
-                    );
-                  }
-                )}
-              </tbody>
-            </TableStyle>
-          </TracksTableWrapper>
-     </>
-      )}
-    </>
-  );
-};
+//       {tracks?.length !== 0 && !isLoading && !error && (
+//         <>
+//           <Button
+//             onClick={() => (!isLoadedTracks ? playMusic() : stopMusic())}
+//             type={"button"}
+//             width={"250px"}
+//             height={"50px"}
+//             padding={"16px"}
+//             margintop={"12px"}
+//             text={isLoadedTracks ? "Зупинити" : "Грати музику"}
+//             showIcon={"true"}
+//             // icon={`${symbol}#icon-play`}
+//             icon={
+//               isLoadedTracks
+//                 ? `${symbol}#icon-stop-play`
+//                 : `${symbol}#icon-play`
+//             }
+//           />
+//            <TracksTableWrapper marginTop={tracksTableProps.marginTop}>
+//             <LatestTracks showTitle={tracksTableProps.showTitle}>{title}</LatestTracks>
+//             <TableStyle>
+//               <THeadStyle>
+//                 <tr>
+//                   {rows.map((row, rowindex) => {
+//                     return <RowTitle key={rowindex}>{row}</RowTitle>;
+//                   })}
+//                 </tr>
+//               </THeadStyle>
+//               <tbody>
+//                 {tracks.map(
+//                   ({
+//                     _id,
+//                     trackPictureURL,
+//                     trackName,
+//                     artist,
+//                     trackDuration,
+//                     trackGenre,
+//                     playList,
+//                   }) => {
+//                     return (
+//                       <TrStyle key={_id}>
+//                         <TableCell>
+//                           <TrackCover
+//                             src={BASE_URL + "/" + trackPictureURL}
+//                             alt={trackName}
+//                             width={55}
+//                           />
+//                         </TableCell>
 
-export default TracksTable;
+//                         <TableCell>{trackName}</TableCell>
+//                         <TableCell>{artist}</TableCell>
+//                         <TableCell>{sToStr(trackDuration)}</TableCell>
+//                         <TableCell>{trackGenre}</TableCell>
+
+//                         <TableCell style={{ display }}>
+//                           {playList?.playListName}
+//                         </TableCell>
+//                         <TableCell style={{ display }}><svg width="24" height="24" stroke="#888889">
+//                           <use href={`${symbol}#icon-dots`}></use>
+//                         </svg></TableCell>
+//                       </TrStyle>
+//                     );
+//                   }
+//                 )}
+//               </tbody>
+//             </TableStyle>
+//           </TracksTableWrapper>
+//      </>
+//       )}
+//     </>
+//   );
+// };
+
+// export default TracksTable;
