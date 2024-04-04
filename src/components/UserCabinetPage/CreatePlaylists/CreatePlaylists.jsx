0da68367ea6480e8cbe8de6b44dfcbf42b5d1a1f
Anton Nikolaenko <anton.ck.ua@gmail.com>
2024-04-04 10:47:22 +0300

    added error modal

--- src/components/UserCabinetPage/CreatePlaylists/CreatePlaylists.jsx
+++ src/components/UserCabinetPage/CreatePlaylists/CreatePlaylists.jsx
@@ -16,6 +16,8 @@ import {
 import symbol from "../../../assets/symbol.svg";
 import ControlMyplaylists from "../ControlMyplaylists/ControlMyplaylists";
 import { Modal } from "../../Modal/Modal";
+import { ModalInfoText } from "../../Modal/Modal.styled";
+import { ErrorNotFound } from "../../Errors/Errors";
 import ModalFormMyplaylist from "../ControlMyplaylists/ModalFormMyplaylist";
 
 import useChooseAvatar from "../../../hooks/useChooseAvatar";
@@ -34,6 +36,7 @@ const CreatePlaylists = ({
   const maxLengthInput = 29;
 
   const [showModal, setShowModal] = useState(false);
+  const [showModalErrorCreate, setShowModalErrorCreate] = useState(false);
 
   // const {
   //   data: dataFavorite,
@@ -42,7 +45,12 @@ const CreatePlaylists = ({
 
   const [
     createPlaylist,
-    { isSuccess, isLoading: isLoadingCreatePlaylist, isError },
+    {
+      isSuccess,
+      isLoading: isLoadingCreatePlaylist,
+      isError: isErrorCreatePlaylist,
+      error: errorCreatePlaylist,
+    },
   ] = useCreatePlaylistForUserMutation();
 
   const [avatar, chooseAvatar, resetAvatar] = useChooseAvatar();
@@ -64,6 +72,7 @@ const CreatePlaylists = ({
       closeModal();
     } catch (error) {
       console.log(error);
+      setShowModalErrorCreate(true);
     }
   };
 
@@ -126,6 +135,25 @@ const CreatePlaylists = ({
             minLength={minLengthInput}
             maxLength={maxLengthInput}
           />
+        </Modal>
+      )}
+      {showModalErrorCreate && (
+        <Modal
+          width={"25vw"}
+          height={"25vh"}
+          onClose={() => setShowModalErrorCreate(false)}
+          showCloseButton={true}
+        >
+          <ModalInfoText fontSize={"20px"} marginBottom={"34px"}>
+            {isErrorCreatePlaylist &&
+            errorCreatePlaylist.data?.code === "4091" ? (
+              <ErrorNotFound
+                error={`Плейлист "${errorCreatePlaylist.data?.object}" вже використовується`}
+              />
+            ) : (
+              <ErrorNotFound error={errorCreatePlaylist.data?.message} />
+            )}
+          </ModalInfoText>
         </Modal>
       )}
     </>
