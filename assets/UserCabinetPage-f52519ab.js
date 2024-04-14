41b264ff230fa83e57f3e82cfd1c128a41de8ffa
Anton-ck <Anton-ck@users.noreply.github.com>
2024-04-14 14:50:48 +0000

    deploy: 4ea98c167451433f5bff4e6c651ac9a5acbb6461

--- assets/UserCabinetPage-f52519ab.js
+++ assets/UserCabinetPage-f52519ab.js
@@ -1,4 +1,4 @@
-import{n as r,f as o,g as z,h as J,i as R,r as a,j as e,B as D,M as h,k as $,l as K,m as X,o as Z,R as _,E as ee,p as te,q as oe,s as re,t as ne,v as se,w as ae,x as ie}from"./index-37d97a0f.js";import"./Statistic.styled-483673bb.js";r.p`
+import{n as r,f as o,g as z,h as J,i as R,r as a,j as e,B as D,M as h,k as $,l as K,m as X,o as Z,R as _,E as ee,p as te,q as oe,s as re,t as ne,v as se,w as ae,x as ie}from"./index-d4eba87b.js";import"./Statistic.styled-b796d1ff.js";r.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 1.21;
